9b88d4a87cfbce412563605d586525d16888b83e
“”Nikolai <semin.nikolai@bk.ru>
2021-11-19 16:29:07 +0200

    Xcode project crated

---
+++ HackathonApp/HackathonApp/HackathonAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackathonAppApp.swift
+//  HackathonApp
+//
+//  Created by Nick Semin on 19.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct HackathonAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
